cf4d6d4298e597b25d3f968992c5c6954b806163
Your Name <sudaniy65@gmail.com>
2022-06-15 10:26:09 +0100

    Setting changes

--- pizza/settings.py
+++ pizza/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
