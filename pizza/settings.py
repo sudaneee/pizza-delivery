1b291a71ba0e4142563bfab8ea8dcb3cc876fd06
Your Name <sudaniy65@gmail.com>
2022-06-15 09:36:04 +0100

    Changes ha

--- pizza/settings.py
+++ pizza/settings.py
@@ -105,15 +105,16 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {}
 
-if DEBUG:
+if not DEBUG:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
+
+else:
     DATABASES["default"]={
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-
     }
-else:
-
-    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
