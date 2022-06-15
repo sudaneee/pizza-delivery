72b894806eec2a8f11dee46c2042bb05bc6ee040
Your Name <sudaniy65@gmail.com>
2022-06-15 11:40:16 +0100

    Using True

--- pizza/settings.py
+++ pizza/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+from tkinter import Y
 from decouple import config
 from datetime import timedelta
 import dj_database_url
@@ -106,17 +107,20 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {}
 
-if DEBUG:
-    DATABASES["default"]={
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
+# if DEBUG:
+#     DATABASES["default"]={
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 
 
 
-else:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+# else:
+#     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#     DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
 # Password validation
